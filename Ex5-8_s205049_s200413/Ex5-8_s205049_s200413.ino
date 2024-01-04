5ba0c1487bc51bcf8ac9758aa0b742f24cff83a1
VHL <vhl@icepower.dk>
2024-01-04 11:01:05 +0100

    Exercise 5 færdig

--- Ex5-8_s205049_s200413/Ex5-8_s205049_s200413.ino
+++ Ex5-8_s205049_s200413/Ex5-8_s205049_s200413.ino
@@ -1,11 +1,19 @@
 
+uint8_t incomingByte = 0;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(Serial.available() > 0)
+  {
+    incomingByte = Serial.read();
+    Serial.print("i received: ");
+    Serial.print((char)incomingByte);
+    Serial.println();
+  }
 }
